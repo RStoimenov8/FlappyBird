a62050283b661f6cc01c26064a480ee0ae6f58ab
Rosen Stoimenov <streamer_bg@abv.bg>
2025-02-16 03:27:51 +0200

    Add files via upload

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,289 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.flappyBird = new System.Windows.Forms.PictureBox();
+            this.pipeTop = new System.Windows.Forms.PictureBox();
+            this.pipeBottom = new System.Windows.Forms.PictureBox();
+            this.ground = new System.Windows.Forms.PictureBox();
+            this.GameTimer = new System.Windows.Forms.Timer(this.components);
+            this.ScoreText = new System.Windows.Forms.Label();
+            this.EndText1 = new System.Windows.Forms.Label();
+            this.EndText2 = new System.Windows.Forms.Label();
+            this.GameDesigner = new System.Windows.Forms.Label();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.howToPlayToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.useSpaceButtonToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.authorToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.StoimenovAbout = new System.Windows.Forms.ToolStripMenuItem();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.flappyBird)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pipeTop)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pipeBottom)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ground)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // flappyBird
+            // 
+            this.flappyBird.Image = ((System.Drawing.Image)(resources.GetObject("flappyBird.Image")));
+            this.flappyBird.Location = new System.Drawing.Point(12, 208);
+            this.flappyBird.Name = "flappyBird";
+            this.flappyBird.Size = new System.Drawing.Size(83, 63);
+            this.flappyBird.TabIndex = 4;
+            this.flappyBird.TabStop = false;
+            // 
+            // pipeTop
+            // 
+            this.pipeTop.Image = global::WindowsFormsApp1.Properties.Resources.pipedown;
+            this.pipeTop.Location = new System.Drawing.Point(201, -416);
+            this.pipeTop.Name = "pipeTop";
+            this.pipeTop.Size = new System.Drawing.Size(190, 556);
+            this.pipeTop.TabIndex = 5;
+            this.pipeTop.TabStop = false;
+            // 
+            // pipeBottom
+            // 
+            this.pipeBottom.Image = global::WindowsFormsApp1.Properties.Resources.pipe;
+            this.pipeBottom.Location = new System.Drawing.Point(311, 300);
+            this.pipeBottom.Name = "pipeBottom";
+            this.pipeBottom.Size = new System.Drawing.Size(186, 218);
+            this.pipeBottom.TabIndex = 6;
+            this.pipeBottom.TabStop = false;
+            // 
+            // ground
+            // 
+            this.ground.Image = global::WindowsFormsApp1.Properties.Resources.ground;
+            this.ground.Location = new System.Drawing.Point(0, 515);
+            this.ground.Name = "ground";
+            this.ground.Size = new System.Drawing.Size(337, 86);
+            this.ground.TabIndex = 7;
+            this.ground.TabStop = false;
+            // 
+            // GameTimer
+            // 
+            this.GameTimer.Enabled = true;
+            this.GameTimer.Interval = 15;
+            this.GameTimer.Tick += new System.EventHandler(this.GameTimer_Tick);
+            // 
+            // ScoreText
+            // 
+            this.ScoreText.AutoSize = true;
+            this.ScoreText.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.ScoreText.Location = new System.Drawing.Point(22, 28);
+            this.ScoreText.Name = "ScoreText";
+            this.ScoreText.Size = new System.Drawing.Size(35, 13);
+            this.ScoreText.TabIndex = 8;
+            this.ScoreText.Text = "label1";
+            // 
+            // EndText1
+            // 
+            this.EndText1.AutoSize = true;
+            this.EndText1.BackColor = System.Drawing.Color.Transparent;
+            this.EndText1.Location = new System.Drawing.Point(210, 182);
+            this.EndText1.Name = "EndText1";
+            this.EndText1.Size = new System.Drawing.Size(35, 13);
+            this.EndText1.TabIndex = 9;
+            this.EndText1.Text = "label2";
+            // 
+            // EndText2
+            // 
+            this.EndText2.AutoSize = true;
+            this.EndText2.Location = new System.Drawing.Point(210, 258);
+            this.EndText2.Name = "EndText2";
+            this.EndText2.Size = new System.Drawing.Size(35, 13);
+            this.EndText2.TabIndex = 10;
+            this.EndText2.Text = "label3";
+            // 
+            // GameDesigner
+            // 
+            this.GameDesigner.AutoSize = true;
+            this.GameDesigner.Location = new System.Drawing.Point(210, 315);
+            this.GameDesigner.Name = "GameDesigner";
+            this.GameDesigner.Size = new System.Drawing.Size(35, 13);
+            this.GameDesigner.TabIndex = 11;
+            this.GameDesigner.Text = "label4";
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(170, 398);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(75, 23);
+            this.btnReset.TabIndex = 12;
+            this.btnReset.Text = "Restart";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.GripStyle = System.Windows.Forms.ToolStripGripStyle.Visible;
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.helpToolStripMenuItem,
+            this.aboutToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.RenderMode = System.Windows.Forms.ToolStripRenderMode.Professional;
+            this.menuStrip1.Size = new System.Drawing.Size(497, 24);
+            this.menuStrip1.TabIndex = 13;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.howToPlayToolStripMenuItem});
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // howToPlayToolStripMenuItem
+            // 
+            this.howToPlayToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.useSpaceButtonToolStripMenuItem});
+            this.howToPlayToolStripMenuItem.Name = "howToPlayToolStripMenuItem";
+            this.howToPlayToolStripMenuItem.Size = new System.Drawing.Size(143, 22);
+            this.howToPlayToolStripMenuItem.Text = "How to play?";
+            this.howToPlayToolStripMenuItem.Click += new System.EventHandler(this.howToPlayToolStripMenuItem_Click);
+            // 
+            // useSpaceButtonToolStripMenuItem
+            // 
+            this.useSpaceButtonToolStripMenuItem.Name = "useSpaceButtonToolStripMenuItem";
+            this.useSpaceButtonToolStripMenuItem.Size = new System.Drawing.Size(166, 22);
+            this.useSpaceButtonToolStripMenuItem.Text = "Use Space button";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.authorToolStripMenuItem});
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(52, 20);
+            this.aboutToolStripMenuItem.Text = "About";
+            // 
+            // authorToolStripMenuItem
+            // 
+            this.authorToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.StoimenovAbout});
+            this.authorToolStripMenuItem.Name = "authorToolStripMenuItem";
+            this.authorToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.authorToolStripMenuItem.Text = "Author";
+            this.authorToolStripMenuItem.Click += new System.EventHandler(this.authorToolStripMenuItem_Click);
+            // 
+            // StoimenovAbout
+            // 
+            this.StoimenovAbout.Name = "StoimenovAbout";
+            this.StoimenovAbout.Size = new System.Drawing.Size(180, 22);
+            this.StoimenovAbout.Text = "Stoimenov";
+            this.StoimenovAbout.Click += new System.EventHandler(this.authorToolStripMenuItem_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(33, 398);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(75, 23);
+            this.btnClose.TabIndex = 14;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::WindowsFormsApp1.Properties.Resources.ground;
+            this.pictureBox1.Location = new System.Drawing.Point(170, 515);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(337, 86);
+            this.pictureBox1.TabIndex = 15;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(497, 601);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.GameDesigner);
+            this.Controls.Add(this.EndText2);
+            this.Controls.Add(this.EndText1);
+            this.Controls.Add(this.ScoreText);
+            this.Controls.Add(this.ground);
+            this.Controls.Add(this.pipeBottom);
+            this.Controls.Add(this.pipeTop);
+            this.Controls.Add(this.flappyBird);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.inGameKeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.GameKeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.flappyBird)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pipeTop)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pipeBottom)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ground)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox flappyBird;
+        private System.Windows.Forms.PictureBox pipeTop;
+        private System.Windows.Forms.PictureBox pipeBottom;
+        private System.Windows.Forms.PictureBox ground;
+        private System.Windows.Forms.Timer GameTimer;
+        private System.Windows.Forms.Label ScoreText;
+        private System.Windows.Forms.Label EndText1;
+        private System.Windows.Forms.Label EndText2;
+        private System.Windows.Forms.Label GameDesigner;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem howToPlayToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem authorToolStripMenuItem;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ToolStripMenuItem useSpaceButtonToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem StoimenovAbout;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        bool jumping = false;
+        int pipeSpeed = 4;
+        int gravity = 4;
+        int Inscore = 0;
+
+        public int Inscore1 { get => Inscore; set => Inscore = value; }
+
+        public Form1()
+        {
+            InitializeComponent();
+            EndText1.Text = "Game Over!";
+            EndText2.Text = "Your final score is: " + Inscore;
+            GameDesigner.Text = "Game Designet by Stoimenov!";
+            EndText1.Visible = false;
+            EndText2.Visible = false;
+            GameDesigner.Visible = false;
+            btnReset.Visible = false;
+            btnClose.Visible = false;
+
+        }
+
+        private void GameTimer_Tick(object sender, EventArgs e)
+        {
+            pipeBottom.Left -= pipeSpeed;
+            pipeTop.Left -= pipeSpeed;
+            flappyBird.Top += gravity;
+            ScoreText.Text = "" + Inscore;
+
+            if (pipeBottom.Left < -80)
+            {
+                pipeBottom.Left = 700;
+                Inscore += 1;
+            }
+            else if (pipeTop.Left < -95)
+            {
+                pipeTop.Left = 800;
+                Inscore += 1;
+            }
+            if (flappyBird.Bounds.IntersectsWith(ground.Bounds))
+            {
+                endGame();
+            }
+            else if (flappyBird.Bounds.IntersectsWith(pipeBottom.Bounds))
+            {
+                endGame();
+            }
+            else if (flappyBird.Bounds.IntersectsWith(pipeTop.Bounds))
+            {
+                endGame();
+            }
+        }
+
+        private void inGameKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Space)
+            {
+                jumping = true;
+                gravity = -5;
+            }
+        }
+
+        private void GameKeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Space)
+            {
+                jumping = false;
+                gravity = 5;
+            }
+        }
+
+        private void endGame()
+        {
+            GameTimer.Stop();
+            EndText1.Visible = true;
+            EndText2.Visible = true;
+            EndText2.Text = "Your Final score is: " + Inscore;
+            GameDesigner.Visible = true;
+            btnReset.Visible = true;
+            btnClose.Visible = true;
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            Application.Restart();
+        }
+
+        private void howToPlayToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Use your Space button!");
+        }
+
+        private void authorToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("By Stoimenov!");
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+    }
+}
